df59f8fe4b417b823dc99e9d0ab15f211aeaf04b
doctoey <reactions58@gmail.com>
2023-03-10 13:29:42 +0700

    add string

--- app3.js
+++ app3.js
@@ -2,3 +2,11 @@ console.log('Hello world');
 
 let email = 'cheewathun.l@gmail.com';
 console.log(email);
+
+let firstName = 'Cheewathun';
+let lastName = 'Lerttanapit';
+
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+console.log(fullName.length);
+console.log(fullName.toUpperCase());
