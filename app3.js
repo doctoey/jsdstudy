e1a7b30cea3ebfeb652350eb3b8d9472f62b7863
doctoey <reactions58@gmail.com>
2023-03-10 13:41:55 +0700

    number operator

--- app3.js
+++ app3.js
@@ -11,7 +11,26 @@ console.log(fullName);
 console.log(fullName.length);
 console.log(fullName.toUpperCase());
 
+
 //boolean 
 
 let findLetter = email.includes('@')
 console.log(findLetter);  // true ใน email มี @ เลยได้ true
+
+
+//number
+
+let raduis = 10;
+const pi = 3.14;
+console.log(raduis,pi);
+
+console.log(10+5); //15
+console.log(10-5); //5
+console.log(10*5); //50
+console.log(10/5); //2
+console.log(10%5); //0  หารเอาเศษ เศษ 0
+console.log(10**2); //100 ยกกำลัง
+
+//order operator
+let nnnnnn = 10*2 + 5**2 -7
+console.log(nnnnnn);
