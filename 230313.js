3d08e5335e3b7955b9faf9157eaf455ac19a4622
doctoey <reactions58@gmail.com>
2023-03-13 15:01:43 +0700

    test update

--- 230313.js
+++ 230313.js
@@ -58,8 +58,9 @@ if (dayNumber2 === 0) {
 let password = 'password';
 
 password.length >= 12 
-  ? console.log('password is very strong so WELCOME') 
-: password.length >= 8 ? console.log('password is ok') 
+? console.log('password is very strong so WELCOME') 
+: password.length >= 8
+? console.log('password is ok') 
 : console.log('password is tooshortttt');
 
 
