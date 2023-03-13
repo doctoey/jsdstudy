5693e8c9d1da522a82b361cdbdce9a488cd9779e
Cheewathun Lerttanapit <123890144+doctoey@users.noreply.github.com>
2023-03-13 14:44:47 +0700

    add passcheck

--- 230313.js
+++ 230313.js
@@ -52,3 +52,21 @@ if (dayNumber2 === 0) {
 } else {
   console.log("Invalid day number");
 }
+
+//ex.2
+
+let password = 'password';
+
+password.length >= 12 
+  ? console.log('password is very strong so WELCOME') 
+: password.length >= 8 ? console.log('password is ok') 
+: console.log('password is tooshortttt');
+
+
+if (password.length >= 12) {
+console.log('password is very strong so WELCOME');
+} else if (password.length >= 8) {
+console.log('password is ok');
+} else {
+console.log('password is tooshortttt');
+}
