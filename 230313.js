6d8dfc65923d3874fca331265dab71287c88bdcc
doctoey <reactions58@gmail.com>
2023-03-13 16:25:54 +0700

    add ex in classroom

--- 230313.js
+++ 230313.js
@@ -98,3 +98,31 @@ let koon = (a, b) => a * b;
 let x = koon(3, 5);
 let y = x;
 console.log(x) //15
+
+
+//โจทย์ในห้อง
+/*
+add(a, b)
+subtract(a, b)
+multiply(a, b)
+divide(a, b)
+*/
+
+
+let add = (a, b) => a + b ;
+let subtract = (a, b) => a / b ;
+let multiply = (a, b) => a * b ;
+let divide = (a, b) => a - b;
+
+add(1,2);
+subtract(10, 5)
+multiply(55, 2)
+divide(10, 4)
+
+/*another exercise Create Function Inform---
+console.log("Hi, my name is " + firstName + "I live in " + location + "and enjoy "+hobby);*/
+
+let hi = (firstName,location,hobby) => {
+console.log(`Hi, my name is ${firstName} I live in ${location} and enjoy ${hobby}`);
+}
+hi("eiei", "Yorkkkkk", "playing slepp");
