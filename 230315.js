fb5752536ab536b4c63b3e75ede9540f03e968d8
doctoey <reactions58@gmail.com>
2023-03-15 13:34:33 +0700

    add file

---
+++ 230315.js
@@ -0,0 +1,13 @@
+const number = [3,4];
+
+number.push(5,6)  // ต่อหลัง
+console.log(number)  //[ 3, 4, 5, 6 ]
+
+number.unshift(1,2) // นำไปไว้ข้างหน้า
+console.log(number)   //[ 1, 2, 3, 4, 5, 6 ]
+
+number.splice(2,0,'a')
+console.log(number)      //[ 1, 2, 'a', 3, 4, 5, 6 ]
+
+number.pop() //เอาตัวสุดท้ายออก
+console.log(number) //[ 1, 2, 'a', 3, 4, 5 ]
