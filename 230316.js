0b78922d43dbfb8dcf26d7d509d211f9fb79df44
doctoey <reactions58@gmail.com>
2023-03-16 13:17:48 +0700

    foreach2

--- 230316.js
+++ 230316.js
@@ -8,3 +8,15 @@ number.forEach((item,index) => {
 //23
 //34
 //45
+
+
+letter = ['a','b','c','d','a','c','a'];
+let count = {};
+letter.forEach(item => {
+    if (count[item]){
+        count[item]++;
+    } else {
+        count[item] = 1;
+    }
+})
+console.log(count); //{ a: 3, b: 1, c: 2, d: 1 } //นับจำนวนตัวซ้ำ
