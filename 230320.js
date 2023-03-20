6ea86afa79b5af009db5ee930ab0387b023830ee
doctoey <reactions58@gmail.com>
2023-03-20 10:37:04 +0700

    tryyyy

--- 230320.js
+++ 230320.js
@@ -62,44 +62,44 @@ const vanillaFlavor = new IceCreamMaking("vanilla", "gelato", [
   "caramel suace",
 ]);
 
-console.log(vanillaFlavor.make());
+// console.log(vanillaFlavor.make());
 
-// const mintChocChip = new IceCreamMaking("Mint Chocolate Chip", "ice cream", [
-//   "choclate sauce",
-//   "hazelnut",
-//   "cherry",
-// ]);
+const mintChocChip = new IceCreamMaking("Mint Chocolate Chip", "ice cream", [
+  "choclate sauce",
+  "hazelnut",
+  "cherry",
+]);
 
 // console.log(mintChocChip.make());
 
-// const strawberryYogurt = new IceCreamMaking("strawberry", "frozen yogurt", [
-//   "strawberry",
-//   "whipped cream",
-//   "strawberry sauce",
-// ]);
+const strawberryYogurt = new IceCreamMaking("strawberry", "frozen yogurt", [
+  "strawberry",
+  "whipped cream",
+  "strawberry sauce",
+]);
 
 // console.log(strawberryYogurt.make());
 
 //
-// function showProps(obj, objName) {
-//   let result = "";
-//   for (let i in obj) {
-//     // obj.hasOwnProperty() is used to filter out properties from the object's prototype chain
-//     if (obj.hasOwnProperty(i)) {
-//       result += objName + "." + i + " = " + obj[i] + "\n";
-//     }
-//   }
-//   return result;
-// }
+function showProps(obj, objName) {
+  let result = "";
+  for (let i in obj) {
+    // obj.hasOwnProperty() is used to filter out properties from the object's prototype chain
+    if (obj.hasOwnProperty(i)) {
+      result += objName + "." + i + " = " + obj[i] + "\n";
+    }
+  }
+  return result;
+}
 // console.log(showProps(vanillaFlavor, "vanillaFlavor"));
 
-// function showProps2(obj, objName) {
-//   let result = "";
-//   for ([key, value] of Object.entries(obj)) {
-//     if (obj.hasOwnProperty(key)) {
-//       result += objName + "." + key + " = " + value + "\n";
-//     }
-//   }
-//   return result;
-// }
+function showProps2(obj, objName) {
+  let result = "";
+  for ([key, value] of Object.entries(obj)) {
+    if (obj.hasOwnProperty(key)) {
+      result += objName + "." + key + " = " + value + "\n";
+    }
+  }
+  return result;
+}
 // console.log(showProps2(vanillaFlavor, "vanillaFlavor"));
