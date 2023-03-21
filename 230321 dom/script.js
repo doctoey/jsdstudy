8ae96f7274a44c5dc9250c09d91f84c3318d2a14
doctoey <reactions58@gmail.com>
2023-03-21 13:40:49 +0700

    add nth1,3

--- 230321 dom/script.js
+++ 230321 dom/script.js
@@ -1,3 +1,10 @@
 const gJavascript = document.querySelector('#reading-list li:nth-child(2) .name');
 console.log(gJavascript);
-gJavascript.innerHTML = 'Advanced JavaScript !!!';
+gJavascript.innerHTML = 'Advanced JavaScript !!!'; // nth-child(2) .name คือชิ้นที่ 2 Java Script > Advanced JavaScript !
+
+
+const tryA = document.querySelector('#reading-list li:nth-child(1) .name');
+tryA.innerHTML = 'tryyyyy !!!'; //HTML & CSS > try
+
+const tryB = document.querySelector('#reading-list li:nth-child(3) .name');
+tryB.innerHTML = 'Ahaaaaa !!!';  //The DOM > Ahaaaaa
