7f620d3357b68c33c34be25b7d0642a3bca59140
doctoey <reactions58@gmail.com>
2023-03-21 13:51:58 +0700

    querySelectorAll

--- 230321 dom/script.js
+++ 230321 dom/script.js
@@ -1,5 +1,5 @@
 const gJavascript = document.querySelector('#reading-list li:nth-child(2) .name');
-console.log(gJavascript);
+//console.log(gJavascript);
 gJavascript.innerHTML = 'Advanced JavaScript !!!'; // nth-child(2) .name คือชิ้นที่ 2 Java Script > Advanced JavaScript !
 
 
@@ -8,3 +8,9 @@ tryA.innerHTML = 'tryyyyy !!!'; //HTML & CSS > try
 
 const tryB = document.querySelector('#reading-list li:nth-child(3) .name');
 tryB.innerHTML = 'Ahaaaaa !!!';  //The DOM > Ahaaaaa
+
+
+const readingList = document.querySelectorAll('#reading-list li .name');
+readingList.forEach(function(list){
+    list.textContent += ' book title'
+}) //add book title ลงใน 3 child เลย
