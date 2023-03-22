62458deab10c7cf948124ef14377279487cca219
doctoey <reactions58@gmail.com>
2023-03-22 16:23:01 +0700

    add function

--- 230322 im xe/main.js
+++ 230322 im xe/main.js
@@ -1,7 +1,13 @@
 // normal import
 
-import {double} from "./toExport.js";; //need {}
+import {double, add} from "./toExport.js";; //need {}
 console.log(double(4));
 console.log(double(1));
 console.log(double(5));
 console.log(double(9));
+
+
+console.log(add(5,5));
+console.log(add(5,6));
+console.log(add(5,99));
+console.log(add(5,5444));
--- 230322 im xe/toExport.js
+++ 230322 im xe/toExport.js
@@ -3,3 +3,6 @@ export function double(n) {
     return n*2;
 }
 
+export function add(x, y) {
+    return x + y;
+}
