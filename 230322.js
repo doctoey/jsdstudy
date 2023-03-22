e7befeac4dac9d5ce59f323aca56467a5425e8c1
doctoey <reactions58@gmail.com>
2023-03-22 15:00:41 +0700

    เพิ่มท่า

--- 230322.js
+++ 230322.js
@@ -1,6 +1,6 @@
 //With Null
-/*
-class Users {
+
+class Users1 {
     constructor(email, password, displayName, height, weight, gender, image) {
         this.email = email;
         this.password = password;
@@ -12,22 +12,22 @@ class Users {
     }
     // ... other methods ...
 }
-class ChildUsers extends Users {
+class ChildUsers1 extends Users1 {
     constructor(email, displayName, image) {
         super(email, null, displayName, null, null, null, image);
     }
     // You can add methods specific to ChildUsers here
 }
 // Example usage
-const childUser = new ChildUsers('email@example.com', 'John Doe', 'path/to/image.jpg');
-console.log(childUser.showEmail); // 'email@example.com'
-console.log(childUser.showDisplay); // 'John Doe'
-console.log(childUser.showImage); // 'path/to/image.jpg'
-*/
+const childUser1 = new ChildUsers1('email@example.com', 'John Doe', 'path/to/image.jpg');
+console.log(childUser1.showEmail); // 'email@example.com'
+console.log(childUser1.showDisplay); // 'John Doe'
+console.log(childUser1.showImage); // 'path/to/image.jpg'
+
 
 
 //destructuring without Null
-class Users {
+class Users2 {
     constructor({ email, password, displayName, height, weight, gender, image }) {
         this.email = email;
         this.password = password;
@@ -39,32 +39,53 @@ class Users {
     }
     // ... other methods ...
 }
-class ChildUsers extends Users {
+class ChildUsers2 extends Users2 {
     constructor({ email, displayName, image }) {
         super({ email, displayName, image });
     }
     // You can add methods specific to ChildUsers here
 }
 // Example usage
-const childUser = new ChildUsers({ email: 'email@example.com', displayName: 'John Doe', image: 'path/to/image.jpg' });
-console.log(childUser.showEmail); // 'email@example.com'
-console.log(childUser.showDisplay); // 'John Doe'
-console.log(childUser.showImage); // 'path/to/image.jpg'
+const childUser2 = new ChildUsers2({ email: 'email@example.com', displayName: 'John Doe', image: 'path/to/image.jpg' });
+console.log(childUser2.showEmail); // 'email@example.com'
+console.log(childUser2.showDisplay); // 'John Doe'
+console.log(childUser2.showImage); // 'path/to/image.jpg'
 
 
 
 
 
+//pass object เข้าไป คล้ายกับท่าบน
+class Users3 {
+    constructor(options) {
+        this.email = options.email;
+        this.password = options.password;
+        this.displayName = options.displayName;
+        this.height = options.height;
+        this.weight = options.weight;
+        this.gender = options.gender;
+        this.image = options.image;
+    }
+    // ... other methods ...
+}
 
+class ChildUsers3 extends Users3 {
+    constructor(options) {
+        super({
+            email: options.email,
+            displayName: options.displayName,
+            image: options.image,
+        });
+    }
 
+    // You can add methods specific to ChildUsers here
+}
 
-
-
-
-
-
-
-
+// Example usage
+const childUser3 = new ChildUsers3({ email: 'email@example.com', displayName: 'John Doe', image: 'path/to/image.jpg' });
+console.log(childUser3.showEmail); // 'email@example.com'
+console.log(childUser3.showDisplay); // 'John Doe'
+console.log(childUser3.showImage); // 'path/to/image.jpg'
 
 
 
