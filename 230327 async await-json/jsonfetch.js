55833e3f9653ff18fb9f380a27a2fe8aff78d728
doctoey <reactions58@gmail.com>
2023-03-27 14:00:22 +0700

    edit json

--- 230327 async await-json/jsonfetch.js
+++ 230327 async await-json/jsonfetch.js
@@ -1,4 +1,4 @@
-fetch('https://reqres.in/api/users/2')
+fetch('https://reqres.in/api/users')//https://reqres.in/api/users/2
 .then((response) => response.json())
 .then((data) => {
   console.log('Success:', data);
