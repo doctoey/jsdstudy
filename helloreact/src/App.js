0a413ec8838f022179a2288e8e7e0e12cf0d4966
Cheewathun Lerttanapit <reactions58@gmail.com>
2023-04-03 13:48:43 +0700

    add study

--- helloreact/src/App.js
+++ helloreact/src/App.js
@@ -2,6 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import React from "react";
 
+const hello = "Hello, world!";
+const number = 26;
+//number % 2 === 0 ? "even" : "odd"
+
+
 function App() {
   return (
     <div className="App">
@@ -10,7 +15,16 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>Hello World</p>
+        <div>Hello Ja</div>{/*Hello Ja*/}
+        <div>{hello}</div>{/*Hello, world!*/}<br/>
+
+        <div>{2+2}</div> {/*4*/}
+        <div>2+2</div>{/*2+2*/}<br/>
+
+        <div>{number}</div>{/*26*/}
+        <div>{number % 2 === 0 ? "is even" : "is odd"}</div><br/>{/*is even*/}
+        
+
         <a
           className="App-link"
           href="https://reactjs.org"
