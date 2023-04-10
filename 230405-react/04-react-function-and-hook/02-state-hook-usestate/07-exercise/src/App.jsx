be3727e85705fa8eb7cff99eee974507acaa17c4
Cheewathun Lerttanapit <reactions58@gmail.com>
2023-04-10 15:20:51 +0700

    buttonnnnn

--- 230405-react/04-react-function-and-hook/02-state-hook-usestate/07-exercise/src/App.jsx
+++ 230405-react/04-react-function-and-hook/02-state-hook-usestate/07-exercise/src/App.jsx
@@ -2,37 +2,44 @@ import React, { useState } from 'react';
 
 function App() {
     // adding state here.
-
+  const [showMes, setMes] = useState()
+  const [choices, setChoices] = useState()
   const handleClick = (value) => {
     // code here.
+    setChoices(value);
   };
+
+// let DivElement = <div></div>
 
   return (
     <div>
-      <button>Fullname</button>
-      <button>Age</button>
-      <button>Picture</button>
-      <DisplayInfo />
+      <button onClick={() => handleClick('Fullname')} >Fullname</button>
+      <button onClick={() => handleClick('Age')} >Age</button>
+      <button onClick={() => handleClick('Picture')} >Picture</button>
+      {/* <button onClick={() => setChoices('Picture')} >Picture</button> */}
+      {/* <button onClick={handleClick} >Picture</button> */}
+      {/* <button handleClick('Age') >Picture</button> */}
+      <DisplayInfo choices={choices} />
     </div>
   );
 }
 
 function DisplayInfo(props) {
 
-  let ___;
-  if (___ === 'Fullname') {
-    ___ = <h2>John Doe</h2>;
-  } else if (___ === 'Age') {
-    ___ = <h2>30</h2>;
-  } else if (___ === 'Picture') {
-    ___ = <img src="https://via.placeholder.com/150" alt="Placeholder" />;
+  let DivElement;
+  if (props.choices === 'Fullname') {
+    DivElement = <h2>John Doe</h2>;
+  } else if (props.choices === 'Age') {
+    DivElement = <h2>30</h2>;
+  } else if (props.choices === 'Picture') {
+    DivElement = <img src="https://via.placeholder.com/150" alt="Placeholder" />;
   } else {
-    ___ = <p>Please select an option.</p>;
+    DivElement = <p>Please select an option.</p>;
   }
 
   return (
     <div>
-      {___}
+      {DivElement}
     </div>
   );
 }
