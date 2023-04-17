87da89cf72cff30f797dd1d6662da3d17157ded5
Cheewathun Lerttanapit <reactions58@gmail.com>
2023-04-17 16:15:37 +0700

    edit

--- 230405-react/07-mini-project--study/src/Home.jsx
+++ 230405-react/07-mini-project--study/src/Home.jsx
@@ -4,90 +4,28 @@ import { Context } from "./UserContext"
 import Admin from './Admin'
 import User from './User'
 
-
-
 const Home = () => {
-    // You have to use getUserRole() to get role of current user
+    // You have to use currentRole to get role of current user
     // role is specify below
     // admin role is 'admin'
     // user role is 'user'
     // 'other' is who didn't login.
-    // const { getUserRole } = useContext(Context)
-    // example of getUserRole.
-    // return type is always string.
-    const { currentRole } = useContext(Context)
-    // example of currentRole.
-    // const _role = currentRole
-    
-    const [role, setRole] = useState()
-    // const [data, setData] = useState()
-    // let prevdata = data
-    
-    const IniApp = () => {
-        const _role = getUserRole()
-        setRole(_role);
-        // setRole(prevdata++);
+    const { getUserRole } = useContext(Context);
+    const [role, setRole] = useState();
+    const initApp = () => {
+        setRole(getUserRole);
     }
-
-    useEffect(IniApp, [])
-
-
-
-    
-
-
-
-
-
-////////////////////////////////////////////////////////////
-
-//     const { getUserRole, getCurrent } = useContext(Context);
-//   // example of getUserRole.
-//   // const _role = getUserRole()
-//   // return type is always string.
-//   const [role, setRole] = useState();
-
-//   const InitApp = () => {
-//     console.log("initApp");
-//     setRole(getUserRole());
-//     console.log("getUserCurrent", getCurrent());
-//   };
-
-//  const { getUserRole, getCurrent } = useContext(Context);
-//   // example of getUserRole.
-//   // const _role = getUserRole()
-//   // return type is always string.
-//   const [role, setRole] = useState();
-
-//   const InitApp = () => {
-//     console.log("initApp");
-//     setRole(getUserRole());
-//     console.log("getUserCurrent", getCurrent());
-//   };
-
-//   useEffect(InitApp, [getCurrent]);
-
-
-////////////////////////////////////////////////////////////
-
-
-
-
-
-
-
-
-
+    // example of currentRole.
+    // return type is always string.
+    useEffect(initApp, [role]);
     // You have to add condition here according to role.
-    // if (role === 'admin') {
-        if (currentRole === 'admin') {
+    if (role === 'admin') {
         return (
             <Layout>
                 <Admin />
             </Layout>
         )
-        }
-    // }
+    }
 
     return (
         <Layout>
