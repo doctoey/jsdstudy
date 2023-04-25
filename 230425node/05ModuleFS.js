ceae6313f609dc8448addcb30f615259c3e1056d
Cheewathun Lerttanapit <reactions58@gmail.com>
2023-04-25 11:10:11 +0700

    add node

---
+++ 230425node/05ModuleFS.js
@@ -0,0 +1,23 @@
+
+const http = require('http')
+const fs = require('fs')
+const port = 8080
+
+http.createServer(render).listen(port)
+console.log('Server started on port: ' + port)
+
+function render(request, response) {
+    let ctype = {'Content-Type':'text/html'}
+    
+    //Module FS readFile index.html and respond 
+    fs.readFile('html/index.html', (error, content) => {
+        if (!error) {
+            response.writeHead(200, ctype)
+            response.write(content)
+        } else {
+            response.writeHead(404, ctype)
+            response.write(error.message)            
+        }
+        return response.end()        
+    })
+}
