972dc5b0d4ddf6bc48c4ab580bdb10daaaa33ddd
Cheewathun Lerttanapit <reactions58@gmail.com>
2023-05-02 14:05:29 +0700

    done 04

--- 230405-react/08-nodejs/04-exercise/src/main.js
+++ 230405-react/08-nodejs/04-exercise/src/main.js
@@ -8,6 +8,40 @@ const port = 3001;
 // code here.
 const company = [];
 
+webServer.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+webServer.get("/company", (req, res) => {
+  // const companyMock = [
+  //   { 
+  //     companyId: "comp-01", 
+  //     name: "Mock Company 01",
+  //     taxId: "tax-000",
+  //     employees: []
+  //  }
+  // ];
+  // res.json(companyMock);
+  // next();
+  res.json(company)
+});
+
+webServer.post("/company", (req, res) => {
+  // const companyId = req.body.companyId;
+  const companyId = `comp-00${company.length + 1}`;
+  const name = req.body.name;
+  const taxId = req.body.taxId || "";
+  const employees = req.body.employees || [];
+
+  const newCompany = { companyId, name, taxId, employees };
+
+  // const newCompany = {
+
+  // }
+  company.push(newCompany);
+  res.json("Create Company Success")
+});
+
 webServer.listen(port, ipAddress, () => {
   console.log(`Web Application Server is running on ${ipAddress} port ${port}`);
   console.log(`Address: http://${ipAddress}:${port}`);
