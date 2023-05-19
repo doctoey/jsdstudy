291b0232f1a379c7aff15af4b32c79c643c44ad5
Cheewathun Lerttanapit <reactions58@gmail.com>
2023-05-19 20:16:13 +0700

    add hashpassword

--- 230515backend/api/authen/authen-controller.js
+++ 230515backend/api/authen/authen-controller.js
@@ -1,4 +1,6 @@
 const userModel = require("./user-model");
+const bcrypt = require("bcrypt"); //เข้ารหัส password
+// import bcrypt from "bcrypt";
 
 class AuthenController {
   async register(req, res) {
@@ -8,23 +10,34 @@ class AuthenController {
     //2.ซ้ำ ส่ง res กลับไปว่าซ้ำ
     //3.ไม่ซ้ำ สร้าง user ใน database
     //4.พอสร้างเสร็จ ส่ง res กลับไปว่าสำเร็จ
-    const { email, password, firstname, lastname } = req.body;
-    const foundUser = await userModel.find({ email });
-    //ถ้าเจอ เมล์ ที่ซ้ำกัน ใน databaseแปลว่ามีแล้ว
-    if (foundUser.length > 0) {
-      return res.status(400).json("Email already exists");
-    } else {
-      //ถ้าไม่ซ้ำ(ไม่มีเมล์อยู่ก่อนหน้า)
-      const newUser = new userModel({
-        //ปั้มโมเดลลงไปใน database
-        email,
-        password,
-        firstname,
-        lastname,
-      });
-      const newUserData = await newUser.save();
-      console.log(newUserData);
-      res.status(200).json(newUserData);
+    try {
+      const { email, password, firstname, lastname } = req.body;
+      const foundUser = await userModel.find({ email });
+      //ถ้าเจอ เมล์ ที่ซ้ำกัน ใน databaseแปลว่ามีแล้ว
+      if (foundUser.length > 0) {
+        return res.status(400).json("Email already exists");
+      } else {
+        //hash password
+        const saltRounds = 10;
+        const salt = bcrypt.genSaltSync(saltRounds);
+        const hashPassword = bcrypt.hashSync(password, salt);
+        const isMatch = bcrypt.compareSync(password, hashPassword);
+
+        //ถ้าไม่ซ้ำ(ไม่มีเมล์อยู่ก่อนหน้า)
+        const newUser = new userModel({
+          //ปั้มโมเดลลง database
+          email: email,
+          password: hashPassword,
+          firstname: firstname,
+          lastname: lastname,
+        });
+        const newUserData = await newUser.save();
+
+        res.status(200).json({ newUserData });
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json("Internal error");
     }
   }
 }
