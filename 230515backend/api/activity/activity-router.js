4accb6315ce41d8159bcde735113f2931c9bc3ca
Cheewathun Lerttanapit <reactions58@gmail.com>
2023-05-20 14:24:01 +0700

    fix activity can run server

--- 230515backend/api/activity/activity-router.js
+++ 230515backend/api/activity/activity-router.js
@@ -1,3 +1,5 @@
 const router = require('express').Router();
+const ActivityController = require('./activity-controller');
 
-router.get('/')
+router.get('/user_activity', ActivityController.getActivity)
+module.exports = router;
